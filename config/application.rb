d353ad6dec88e6ab4f2686507a22c574b48f0358
Kapil <kapil@idyllic-software.com>
2014-12-05 00:56:40 +0530

    show exception change

--- config/application.rb
+++ config/application.rb
@@ -19,6 +19,9 @@ module Snacks
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
 
+    # Raise exceptions instead of rendering exception templates.
+    config.action_dispatch.show_exceptions = true
+
     # Set Time.zone default to the specified zone and make Active Record auto-convert to this zone.
     # Run "rake -D time" for a list of tasks for finding time zone names. Default is UTC.
     # config.time_zone = 'Central Time (US & Canada)'
