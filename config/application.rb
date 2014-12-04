628b49c6815c9c0bc4a9e6003846190516dc180c
Kapil <kapil@idyllic-software.com>
2014-12-05 01:12:05 +0530

    added show exception

--- config/application.rb
+++ config/application.rb
@@ -21,6 +21,7 @@ module Snacks
 
     # Raise exceptions instead of rendering exception templates.
     config.action_dispatch.show_exceptions = true
+    #comment
 
     # Set Time.zone default to the specified zone and make Active Record auto-convert to this zone.
     # Run "rake -D time" for a list of tasks for finding time zone names. Default is UTC.
